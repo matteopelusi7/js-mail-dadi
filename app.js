a18399ce5744e11869bb8fab5e237bf830f56522
matteopelusi7 <pelusimatteo02@gmail.com>
2022-02-10 13:03:09 +0100

    Modifiche js esercizio 1

--- app.js
+++ app.js
@@ -11,15 +11,23 @@ mailPlay.addEventListener('click', function() {
         'prova2502@hotmail.com',
         'pluto@gmail.com'
     ];
+
+    let find = false;
     
      for (let i = 0; i < mailDataBase.length; i++) {
     
         const element = mailDataBase[i];
     
         if (mailUser == element) {
-            alert('Sei autorizzato all\'accesso!');
+            find = true;
         }
     
+    }
+
+    if ( find == true) {
+        alert('Sei autorizzato all\'accesso!');
+    } else {
+        alert('I dati inseriti non sono corretti');
     }
 
 })
@@ -31,20 +39,20 @@ dadiPlay.addEventListener('click', function() {
 
     const randomUser = Math.floor ( Math.random() * 6 + 1 );
     const numUser = document.getElementById('numero-utente');
-    numUser.innerHTML += `il tuo numero generato è: ${randomUser}`;
+    numUser.innerHTML = `il tuo numero generato è: ${randomUser}`;
 
     const randomComputer = Math.floor ( Math.random() * 6 + 1 );
     const numComputer = document.getElementById('numero-computer');
-    numComputer.innerHTML += `il numero generato dal computer è: ${randomComputer}`;
+    numComputer.innerHTML = `il numero generato dal computer è: ${randomComputer}`;
 
     let result = document.getElementById('risultato');
     
     if (randomUser > randomComputer) {
-        result.innerHTML += 'Hai vinto!'
+        result.innerHTML = 'Hai vinto!';
     } else if (randomUser < randomComputer) {
-        result.innerHTML += 'Hai perso'
+        result.innerHTML = 'Hai perso';
     } else {
-        result.innerHTML += 'Abbiamo pareggiato'
+        result.innerHTML = 'Abbiamo pareggiato';
     }
 
 })
